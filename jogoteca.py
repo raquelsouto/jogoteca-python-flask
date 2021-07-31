6acd4a9e9b64d40d713b182b37d23aafec6104c1
Raquel Souto <kelsouto@gmail.com>
2021-07-31 01:52:02 -0300

    Atualizar projeto

--- jogoteca.py
+++ jogoteca.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 class Jogo:
@@ -9,12 +8,30 @@ class Jogo:
         self.console = console
 
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
-    lista = [jogo1, jogo2, jogo3]
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+lista = [jogo1, jogo2, jogo3]
+
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogo', jogos=lista)
+
 
 app.run(debug=True)
