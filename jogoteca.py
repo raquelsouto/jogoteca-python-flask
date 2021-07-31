af8211c2103d8e471f6080ec1fe9109ecd293566
Raquel Souto <kelsouto@gmail.com>
2021-07-31 00:57:56 -0300

    Listar jogos do projeto

--- jogoteca.py
+++ jogoteca.py
@@ -2,8 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run(debug=True)
